980f444de9a420c51be23176a3bf10c34b6b3c58
EleonoraFlaborea <flaboreaeleonora@gmail.com>
2024-03-13 17:01:42 +0100

    first commit

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <link rel="icon" type="image/svg+xml" href="./src/img/nero.png" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title> Comics | @yield('title')</title>
+
+{{--CDNS--}}
+@yield('cdns')
+
+{{--Assets vite--}}
+@vite('resources/js/app.js')
+
+</head>
+
+<body>
+{{--HEADER--}}
+
+{{--MAIN--}}
+{{--FOOTER--}}
+  <footer></footer>
+
+</body>
+
+</html>
