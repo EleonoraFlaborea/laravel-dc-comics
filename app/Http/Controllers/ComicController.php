ca916dd0637beb8701bd314515ed9ddfce16f051
EleonoraFlaborea <flaboreaeleonora@gmail.com>
2024-03-18 19:26:00 +0100

    add routes

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -6,7 +6,8 @@ use Illuminate\Http\Request;
 
 class ComicController extends Controller
 {
+    //funzione per mostrare le lista dei comics
     public function index(){
-        
+
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -19,11 +19,4 @@ Route::get('/', function () {
 })->name('home');
 
 Route::get('/comics', [ComicController::class, 'index'])->name('comics.index');
-
-/*Route::get('/movies', function () {
-    return view('movies');
-})->name('movies');
-
-Route::get('/shop', function () {
-    return view('shop');
-})->name('shop');*/
+Route::get('/comics/{comic}', [ComicController::class, 'show'])->name('comics.show');
